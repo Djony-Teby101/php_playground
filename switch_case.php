273b58a1c6c9121a307aff4ccae8f6fecc7520cc
teby lorince <111266131+Djony-Teby101@users.noreply.github.com>
2026-03-15 13:09:07 +0100

    Update switch_case.php
    amelioration du script.

--- switch_case.php
+++ switch_case.php
@@ -1,22 +1,40 @@
 <?php 
 
-$type_utilisateur="Admin";
+// Gestionnaire des erreurs.
+$erreur=[];
 
-switch ($type_utilisateur) {
-    case 'Admin':
-        var_dump("welcome Admin");
-        break;
+// type input form data.
+$type_utilisateur=['Admin', 'Conducteur', 'Utilisateur'];
+$session_connect= shuffle($type_utilisateur);
+
+$session_connect=$type_utilisateur[0];
+$session_connect=strtolower($session_connect);
+
+
+
+try {
+
+    switch ($session_connect) {
     
-    case 'Conducteur':
-        var_dump("profile redacteur");
-        break;
-    case 'utilisateur':
-        var_dump("welcome here");
-        break;
-    
-    default:
-       var_dump("un probleme est survenu");
-        break;
+        case 'admin':
+            var_dump("welcome Admin");
+            break;
+        
+        case 'conducteur':
+            var_dump("profile conducteur.");
+            break;
+        case 'utilisateur':
+            var_dump("Bienvenu sur votre profile.");
+            break;
+        
+        default:
+        var_dump("un probleme est survenu.");
+            break;
+    }
+} catch (PDOException $e) {
+   $erreur[]="une erreur est survenu";
+   return $erreur;
 }
+
 
 ?>
